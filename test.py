94b6140ceca2c86fc3f8684cb7a8fae40d7eeac8
Lin97140 <79233345+Lin97140@users.noreply.github.com>
2022-04-09 10:43:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,30 +6,34 @@ import jieba
 headers = {"content-type": "text/html; charset=UTF-8","user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"}
 n = 51
 file_count = 469
+
+
+#從goodlife.tw抓資料
 for i in range(n, 117):
     print(i)
     if i == 1:
         url = "https://goodlife.tw/%E5%92%96%E5%95%A1%E5%BA%97/%E6%98%9F%E5%B7%B4%E5%85%8B"
     else:
-        url = "https://goodlife.tw/%E5%92%96%E5%95%A1%E5%BA%97/%E6%98%9F%E5%B7%B4%E5%85%8B/page/" +str(i)
+        url = "https://goodlife.tw/%E5%92%96%E5%95%A1%E5%BA%97/%E6%98%9F%E5%B7%B4%E5%85%8B/page/" +str(i) #每一頁的網址
     res = re.get(url, headers = headers)
     soup = Bs(res.text, "html.parser")
-    result = soup.find_all("li", class_="topic expired")
+    result = soup.find_all("li", class_="topic expired") #抓每個標題
+    
     for r in result:
         if r.find("a"):
-            href = r.find("a")["href"]
-            title = r.find("a").text
+            href = r.find("a")["href"]  #抓每個標題的連結
+            title = r.find("a").text    #抓標題文字
             
             url = href
             res2 = re.get(url, headers = headers)
             soup2 = Bs(res2.text, "html.parser")
             if soup2.find("div", class_="article"):    
-                text = soup2.find("div", class_="article")
-            with open(file="data\\starbucks\\starbucks"+str(file_count)+".txt", mode="w", encoding="UTF-8") as file:
+                text = soup2.find("div", class_="article") #抓每個標題的內容
+            with open(file="data\\starbucks\\starbucks"+str(file_count)+".txt", mode="w", encoding="UTF-8") as file: #將標題和內容寫入文字檔
                 text_cut = jieba.cut(text.text)
                 file.write(title+'\n')
                 file.write(" ".join(text_cut))
                 file_count += 1
     if i % 5 == 0:
-        time.sleep(random.randint(2,5))
+        time.sleep(random.randint(2,5)) #每五頁暫停2~5秒
         
